48fabe3cb79b3c648cd4ed19677844f8e571b6a9
AaronNazareth <4nm20cs005@nmamit.in>
2022-10-20 21:56:48 +0530

    Aaron

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Description from './Components/Description.js';
+import Header from './Components/Header.js'
+
+function App() {
+  return (
+    <><Header />
+    <Description /></>
+  );
+}
+
+export default App;

bf25937c5aec945e652f8c3f57c8dcd8c7ef474d
AaronNazareth <4nm20cs005@nmamit.in>
2023-01-21 12:37:45 +0530

    some minor changes

--- src/Components/Description.js
+++ src/Components/Description.js
@@ -1,7 +1,15 @@
+import skills from "./Data/SkillsData";
+import "./css/Description.css";
+
 function Description(){
     return <>
     <div className="Description">
-        <p>Aloha, I am Computer Science Engineering Student. My interests are web development, watching anime, movies, random things on Youtube. I am also a pianist and like to play during my free time.</p>
+        <span>Aloha, My name is</span>
+        <h1>Aaron Jevil Nazareth</h1>
+        <h1>I build pages in <span className="mov-text">{Array(skills.length).fill().map( (_,i) =>(
+            <li className="moving-text">{ skills[i].title }</li>
+        ))}</span></h1>
+        <p>I am Computer Science Engineering Student. My interests are web development, watching anime, movies, random things on Youtube. I am also a pianist and like to play during my free time.</p>
     </div>
     </>
 }

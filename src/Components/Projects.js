65edb683bfe7daa6b0c740d7a645cfb39e30ce75
AaronNazareth <4nm20cs005@nmamit.in>
2023-01-25 22:29:11 +0530

    added link icon

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,7 +4,7 @@ import './css/Projects.css';
 function Projects({ Key,title,description,frameworks,github,live }) {
   return (
       <div className="projects">
-        <h2 className="projecttitle">{title}<span><a href={github}><i class="fa-brands fa-github"></i></a></span></h2>
+        <h2 className="projecttitle">{title}<span><a href={github}><i class="fa-brands fa-github"></i></a></span><span><a href={live}><i class="fa-solid fa-link"></i></a></span></h2>
         <p className="projectdes">{description}</p>
         <div className="projectList">
             {Array(frameworks.length).fill().map((_,i)=>(

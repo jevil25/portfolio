c9766af8e6fd180b62c49fee3d01cd671870938f
AaronNazareth <4nm20cs005@nmamit.in>
2023-01-28 21:55:15 +0530

    added resume link

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -28,7 +28,7 @@ function Header(){
                 </div>
             </div>
             <div ata-aos="fade-right" data-aos-duration="1000" className={`navResume ${!isOpen ? 'closeNav':""}`}>
-                <a href="https://drive.google.com/uc?id=12ZTuCrEgXhhmACwGBJGi_cpDKohIcfDR&export=download">Resume</a>
+                <a href="https://drive.google.com/uc?id=18hywkm-rr8uqiWEWpdPpyaLwKZMUYS87&export=download">Resume</a>
             </div>
         </div>
     </div>

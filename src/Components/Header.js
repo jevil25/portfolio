9cd1ee50fea19b1b8c7d5d841e0c7b6554fc18cc
AaronNazareth <4nm20cs005@nmamit.in>
2023-01-28 12:41:45 +0530

    made navbar responsive

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import './css/Header.css';
+import { useState } from 'react';
 const logoImage = require("./Images/loader.png")
 
+
 function Header(){
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => {
+        setIsOpen(!isOpen);
+    }
+
     return (<>
     <div className="header">
-        <div className="navbar">
+        <div className={`navbar ${!isOpen ? '':"navbar-padding"}`}>
             <div className="navlist">
                 <a href="#Description" className='AJN'><img src={logoImage} alt="" /><span>Aaron Nazareth</span></a>
-                <a href="#Description">About</a>
-                <a href="#Projects">Projects</a>
-                <a href="#Skills">Skills</a>
-                <a href="#Contact">Contact</a>
+                <button onClick={toggle} class="navbar-toggle">
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div className={`mainnav ${!isOpen ? 'closeNav':""}`}>
+                    <a href="#Description">About</a>
+                    <a href="#Projects">Projects</a>
+                    <a href="#Skills">Skills</a>
+                    <a href="#Contact">Contact</a>
+                </div>
             </div>
-            <div className="navResume">
+            <div className={`navResume ${!isOpen ? 'closeNav':""}`}>
                 <a href="https://drive.google.com/uc?id=12ZTuCrEgXhhmACwGBJGi_cpDKohIcfDR&export=download">Resume</a>
             </div>
         </div>

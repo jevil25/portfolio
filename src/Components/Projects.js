e88b82e04d36c7058ee120cec1f9f59532e2257d
AaronNazareth <4nm20cs005@nmamit.in>
2023-06-13 20:13:01 +0530

    feat:project component redesign

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './css/Projects.css';
 import { useState } from 'react';
+import { motion } from "framer-motion";
 
 function Projects({ Key,title,description,frameworks,github,live,pic,colors }) {
   // console.log(title)
   const [desClass, setDesClass] = useState('hideDes')
+  const [width, setWidth] = useState("");
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  const handleHover = (e) => {
+    if(screenWidth < 600) return;
+    console.log('hover')
+    setWidth("width")
+    setDesClass('showDes')
+  }
+
+  const handleLeave = (e) => {
+    if(screenWidth < 600) return;
+    console.log('leave')
+    setDesClass('hideDes')
+    setWidth("")
+  }
+
+  useEffect(() => {
+    const handleResize = () => setScreenWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    setDesClass("showDes")
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-      <div data-aos="fade-right" data-aos-duration="1000" className="projects">
-        <div className="project_body">
+      <motion.div 
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}  
+        className={`projects `}
+        onMouseEnter={e => handleHover(e)}
+        onMouseLeave={e => handleLeave(e)}
+      >
+        <motion.div  className="project_body">
           {pic.map((pic)=>
           ( <div className='project_image'>
             <a href={live}><img src={pic} alt={title} /></a>
@@ -27,16 +59,16 @@ function Projects({ Key,title,description,frameworks,github,live,pic,colors }) {
               </a>)
               )}
             </h2>
-            <h3 class="projectStack">Tech Stack</h3>
             <p className={`projectDes ${desClass}`}>{description}</p>
+            <h3 class="projectStack">Tech Stack</h3>
             <div className="projectList">
               {Array(frameworks.length).fill().map((_,i)=>(
                   <li className='each_skill2' style={{backgroundColor: "#213b61"}}>{frameworks[i]}</li>
               ))}
             </div>
           </div>
-        </div>
-    </div>
+        </motion.div>
+    </motion.div>
   )
 }
 

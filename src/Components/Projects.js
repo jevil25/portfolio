7725cf54a514b4b6813c65fc3af9137aea7c28cd
AaronNazareth <4nm20cs005@nmamit.in>
2023-06-25 15:41:21 +0530

    new design

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,51 +4,25 @@ import { useState } from 'react';
 import { motion } from "framer-motion";
 
 function Projects({ Key,title,description,frameworks,github,live,pic,colors }) {
-  // console.log(title)
-  const [desClass, setDesClass] = useState('hideDes')
-  const [width, setWidth] = useState("");
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-
-  const handleHover = (e) => {
-    if(screenWidth < 600) return;
-    console.log('hover')
-    setWidth("width")
-    setDesClass('showDes')
-  }
-
-  const handleLeave = (e) => {
-    if(screenWidth < 600) return;
-    console.log('leave')
-    setDesClass('hideDes')
-    setWidth("")
-  }
-
-  useEffect(() => {
-    const handleResize = () => setScreenWidth(window.innerWidth);
-    window.addEventListener("resize", handleResize);
-    if(screenWidth < 600) setDesClass("showDes");
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
   return (
       <motion.div 
-        initial={{ opacity: 0, scale: 0.5 }}
+        initial={{ opacity: 0.5, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 0.5 }}  
+        transition={{ duration: 1 }}  
         className={`projects `}
-        onMouseEnter={e => handleHover(e)}
-        onMouseLeave={e => handleLeave(e)}
       >
         <motion.div  className="project_body">
-          {pic.map((pic)=>
-          ( <div className='project_image'>
-            <a href={live}><img src={pic} alt={title} /></a>
-          </div>)
-          )}
-          <div>
+          <div class="project1">
             <h2 className="projecttitle">
               {title}
-              {github.map((github)=>
+            </h2>
+            <p className={`projectDes`}>{description}</p>
+            <div className="projectList">
+              {Array(frameworks.length).fill().map((_,i)=>(
+                  <li className='each_skill2'>{frameworks[i]}</li>
+              ))}
+            </div>
+            {github.map((github)=>
               (<a href={github}>
                 <i class="fa-brands fa-github spacing"></i>
               </a>)
@@ -58,15 +32,12 @@ function Projects({ Key,title,description,frameworks,github,live,pic,colors }) {
                 <i class="fa-solid fa-link spacings"></i>
               </a>)
               )}
-            </h2>
-            <p className={`projectDes ${desClass}`}>{description}</p>
-            <h3 class="projectStack">Tech Stack</h3>
-            <div className="projectList">
-              {Array(frameworks.length).fill().map((_,i)=>(
-                  <li className='each_skill2' style={{backgroundColor: "#213b61"}}>{frameworks[i]}</li>
-              ))}
-            </div>
           </div>
+          {pic.map((pic)=>
+          ( <div className='project_image'>
+            <a href={live}><img src={pic} alt={title} /></a>
+          </div>)
+          )}
         </motion.div>
     </motion.div>
   )

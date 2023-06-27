ade7b0e969735edf44358f74aa46f44cf4c42850
AaronNazareth <4nm20cs005@nmamit.in>
2023-06-27 12:43:24 +0530

    feat:refactor

--- src/Components/Data/experience.js
+++ src/Components/Data/experience.js
@@ -1,6 +1,14 @@
 const experience = [
     {
         id: 1,
+        company: "Salesine Technologies",
+        designation: "Web Developer",
+        duration: "June 2023 - Present",
+        description: "Working as a Full Stack Developer in a team. I am working on a sales page which can record zoom meetings send it ai model and give various anlytics and display them such as sentiment analysis, keywords, etc. I am working on the front end of the application using ReactJS and the back end using NodeJS and ExpressJS with MongoDB as the database. I am also working on the deployment of the application using vercel.",
+        certificate: ""
+    },
+    {
+        id: 2,
         company: "Dream Bharat Foundation",
         designation: "Web Developer",
         duration: "January 2023 - February 2023",
@@ -8,7 +16,7 @@ const experience = [
         certificate: "https://drive.google.com/file/d/1oZa1VVh5FL6-yuWNZkD2VtOCEn-2_cYk/view?usp=share_link"
     },
     {
-        id: 2,
+        id: 3,
         company: "Sparks Foundation",
         designation: "web Developer",
         duration: "November 2023 - December 2023",

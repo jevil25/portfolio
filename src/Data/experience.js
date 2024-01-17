b8a8efcd86f327417cccefa7d351c652d1798a43
AaronNazareth <jevil257@gmail.com>
2024-01-17 11:04:08 +0530

    feat:deatails update

--- src/Data/experience.js
+++ src/Data/experience.js
@@ -1,6 +1,14 @@
 const experience = [
     {
-        id: 1,
+        id:1,
+        company: "Louisa Technologies",
+        designation: "Software Engineer Intern",
+        duration: "Jan 2024 - July 2024",
+        description: "Working as a Full Stack Developer in a team.",
+        certificate: ""
+    },
+    {
+        id: 2,
         company: "Salesine Technologies",
         designation: "Web Developer",
         duration: "June 2023 - Present",
@@ -8,20 +16,12 @@ const experience = [
         certificate: "https://drive.google.com/file/d/1TTbRgzAPnTyaO34V5zTBMWXvm4YkzMcU/view"
     },
     {
-        id: 2,
+        id: 3,
         company: "Dream Bharat Foundation",
         designation: "Web Developer",
         duration: "January 2023 - February 2023",
         description: "Working as a Full Stack Developer in the DBF team. I was working on a medicine webpage. The application is used to sell medicines. I was working on the front end of the application using ReactJS and the back end using NodeJS and ExpressJS with MongoDB as the database. I was also working on the deployment of the application using vercel.",
         certificate: "https://drive.google.com/file/d/1oZa1VVh5FL6-yuWNZkD2VtOCEn-2_cYk/view?usp=share_link"
-    },
-    {
-        id: 3,
-        company: "Sparks Foundation",
-        designation: "web Developer",
-        duration: "November 2023 - December 2023",
-        description: "Working as a Full Stack Developer in the Sparks Digital team. I was working on banking web page which was being developed for a client of the company. I made the page using handlebars,MongoDB,ExpressJS,NodeJS and deployed it using heroku.",
-        certificate: "https://drive.google.com/file/d/1RuNexp_CxMbw1EHPWNSpbCvFGtMb_3uf/view?usp=sharing"
     },
 ]
 
